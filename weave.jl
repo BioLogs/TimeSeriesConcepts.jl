a5aff73e265c16b60e9d1509fd207e627ed1fff2
edwinb-ai <developeredwin@gmail.com>
2020-04-07 19:36:57 -0500

    :construction:
    WIP part 3 of the introduction

--- weave.jl
+++ weave.jl
@@ -1,16 +1,20 @@
 using Weave
 
 # ! Create a collection of files to process
-filelist = ["concepts-p1", "concepts-p2"]
+filelist = [
+    "concepts-p1",
+    "concepts-p2",
+    "concepts-p3",
+]
 
 function convert_weave(files::String)
     # ! Loop over all of them to convert between types
     filejl = "scripts/" * files * ".jl"
     filejmd = "markdown/" * files * ".jmd"
     # * From jl to jmd
-    # convert_doc(joinpath("src/", filejl), joinpath("src/", filejmd))
+    convert_doc(joinpath("src/", filejl), joinpath("src/", filejmd))
     # * From jmd to jl
-    convert_doc(joinpath("src/", filejmd), joinpath("src/", filejl))
+    # convert_doc(joinpath("src/", filejmd), joinpath("src/", filejl))
 
     # ! Finally, weave just the script files and return a HTML file
     weave(joinpath("src/", filejl), out_path = "build")
