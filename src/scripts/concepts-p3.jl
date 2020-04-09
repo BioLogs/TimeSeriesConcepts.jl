2251a192a3e963367372cca1b0d8fd8c734e100d
edwinb-ai <developeredwin@gmail.com>
2020-04-08 22:57:21 -0500

    :construction:
    Part 3 is still WIP but most of the code is now done, also updated
    some function definitions for that part, next up is writing most of
    the document and information in general.

--- src/scripts/concepts-p3.jl
+++ src/scripts/concepts-p3.jl
@@ -18,44 +18,61 @@ dates = DateTime(2018, 1, 1, 1):Dates.Minute(1):DateTime(2018, 12, 31, 24)
 xt = randn(rng, length(dates))
 
 #+
-ar = @. xt[1:(end-1)] - 0.7 * xt[2:end]
+ar = 5.0 .+ xt[1:(end-1)] - 0.7 * xt[2:end]
+# Note that we need to remove the last element from the dates
 df_ar = TimeArray(dates[1:(end-1)], ar) |> DataFrame
+# We can now plot the time series, only 100 elements though
 @df df_ar plot(:timestamp[1:100], :A[1:100])
 
 #+
-println(cov(df_ar[:, :A], df_ar[:, :A]))
+true_γ = 1.49
+
+#+
+cov(df_ar[:, :A], df_ar[:, :A])
 
 #+ results = "hidden"
 function avf(x::AbstractArray, h::Integer)
+    # Get the total number of elements in the time series
     n = length(x)
     # Compute sample mean
-    μ = mean(x)
+    μ = sum(x) / n
+    # Initialize the sample autocovariance
     γ = 0.0
 
+    # Get the final index according to the lag
     endidx = Int(n - h)
+    # Use the definition for the sample autocovariance
     @inbounds for i in 1:endidx
         γ += (x[i + h] - μ) * (x[i] - μ)
     end
 
-    return γ / (n - h)
+    # And normalize
+    return γ / n
 end
 
-#+ results = "hidden"
-# true_γ = 1 / 9
-# γ_hand = avf(df[:, :A], 2)
-# γ_jl = cov(df[1:(end - 2), :A], df[3:end, :A])
-
-#+ hold = true
-# println("True autocovariance value: $true_γ")
-# println("AVF by hand: $γ_hand")
-# println("AVF using Julia: $γ_jl")
+#+
+γ_hand = avf(df_ar[:, :A], 0)
 
 #+ results = "hidden"
 function acrf(x::AbstractArray, h::Integer)
-    return avf(x, h) / avf(x, 0)
+    # Get the total number of elements in the time series
+    n = length(x)
+    # Obtain the sample mean
+    μ = sum(x) / n
+
+    # Compute the autocovariance for a time series
+    # without lag
+    autocov = sum((x[i] - μ)^2 for i = eachindex(x))
+    # Normalize
+    autocov /= n
+
+    # Get the autocovariance with lag and divide it
+    # by the one without lag
+    return avf(x, h) / autocov
 end
 
-#+ results = "hidden"
-# true_ρ = 1 / 3
-# ρ_hand = acrf(df[:, :A], 2)
-# ρ_jl = cor(df[1:(end - 2), :A], df[3:end, :A])
+#+
+ρ_hand = acrf(df_ar[:, :A], 1)
+
+#+
+ρ_jl = cor(df_ar[1:(end - 1), :A], df_ar[2:end, :A])
