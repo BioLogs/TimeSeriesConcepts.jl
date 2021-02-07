d4dcb8e21a2aa5c8aff69e394ce91d4184d8b4c9
Edwin Bedolla <developeredwin@gmail.com>
2021-02-07 16:45:08 -0600

    :sparkles: :art: :fire:
    Started the migration to Pluto.

---
+++ src/scripts/concepts2.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0edc349e-6996-11eb-17fe-75948b35db2b
+
+
+# ╔═╡ Cell order:
+# ╠═0edc349e-6996-11eb-17fe-75948b35db2b
